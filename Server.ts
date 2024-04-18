a25a6a1e652b09abaac72fb37a541c71cc388284
Tal Ginsberg <131457400+talgins47@users.noreply.github.com>
2024-04-18 15:35:37 +0300

    Delete Server.ts

--- Server.ts
+++
@@ -1,9 +0,0 @@
-import init from "./App";
-
-init().then((app) => {
-  app.listen(process.env.PORT, () => {
-    console.log(
-      "Example app listening at http://localhost:" + process.env.PORT
-    );
-  });
-});
